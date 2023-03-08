a9b480072b234bf0867f78b74febf43666471f3d
Yoo KyeongMi <89245114+YooKyungmi@users.noreply.github.com>
2023-03-08 23:05:42 +0900

    feat: 특정 게시물 조회 API 구현 최종

--- api-server-spring-boot-jdbc/src/main/java/com/example/demo/src/post/PostController.java
+++ api-server-spring-boot-jdbc/src/main/java/com/example/demo/src/post/PostController.java
@@ -1,9 +1,47 @@
 package com.example.demo.src.post;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.demo.config.BaseException;
+import com.example.demo.config.BaseResponse;
+import com.example.demo.src.post.model.GetPostRes;
+import com.example.demo.utils.JwtService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/app/post")
+@RequestMapping("/app/posts")
 public class PostController {
+    final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @Autowired
+    private final PostProvider postProvider;
+    @Autowired
+    private final PostService postService;
+    @Autowired
+    private final JwtService jwtService;
+
+    public PostController(PostProvider postProvider,PostService postService,JwtService jwtService){
+        this.postProvider=postProvider;
+        this.postService = postService;
+        this.jwtService=jwtService;
+    }
+
+    /**
+     * 특정 게시물 조회 API
+     * [GET] /app/posts/:post-id
+     * @return
+     */
+
+    @ResponseBody
+    @GetMapping("/{post-id}")
+    public BaseResponse<GetPostRes> getPost(@PathVariable("post-id") int postId){
+        try{
+            GetPostRes getPostRes = postProvider.getPost(postId);
+            return new BaseResponse<>(getPostRes);
+        } catch(BaseException exception){
+            return new BaseResponse<>((exception.getStatus()));
+        }
+    }
+
 }
