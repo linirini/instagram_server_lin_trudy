10eb0d5f0cb0d20b8b08801b0f2b5c6383386983
Yoo KyeongMi <89245114+YooKyungmi@users.noreply.github.com>
2023-03-12 22:14:59 +0900

    댓글 작성 api:

---
+++ api-server-spring-boot-jdbc/src/main/java/com/example/demo/src/post/model/comment/PostCommentReq.java
@@ -0,0 +1,21 @@
+package com.example.demo.src.post.model.comment;
+
+import lombok.*;
+
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class PostCommentReq {
+    @NotNull(message ="postId는 필수 입력요소 입니다")
+    private int postId;
+    private int groupId;
+    @NotEmpty(message ="comment는 필수 입력요소 입니다.")
+    private String comment;
+    private List<Integer> mention;
+}
