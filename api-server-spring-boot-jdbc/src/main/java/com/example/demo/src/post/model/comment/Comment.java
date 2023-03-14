7bb26413e209bbdaf97da23b13ffd5b2467b245a
Yoo KyeongMi <89245114+YooKyungmi@users.noreply.github.com>
2023-03-14 17:25:33 +0900

    오류 수정

--- api-server-spring-boot-jdbc/src/main/java/com/example/demo/src/post/model/comment/Comment.java
+++ api-server-spring-boot-jdbc/src/main/java/com/example/demo/src/post/model/comment/Comment.java
@@ -1,9 +1,6 @@
 package com.example.demo.src.post.model.comment;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import javax.validation.constraints.NotEmpty;
 
@@ -11,15 +8,12 @@ import javax.validation.constraints.NotEmpty;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 public class Comment {
-    @NotEmpty
     private int commentId;
-    @NotEmpty
     private int postId;
-    @NotEmpty
     private int userId;
     private int groupId;
-    @NotEmpty
     private String comment;
     private int status;
     private String createdAt;
